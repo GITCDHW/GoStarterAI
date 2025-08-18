486e47b30f2a3f1212df67c6627c15ef113fbcea
GITCDHW <pank36720@gmail.com>
2025-08-18 14:02:57 +0530

    update agent_1.js

--- netlify/functions/agent_1.js
+++ netlify/functions/agent_1.js
@@ -25,8 +25,9 @@ const handler = async (event, context) => {
     }
     try {
         const { userPrompt } = JSON.parse(event.body)
+        console.log(userPrompt)
         const prompt = `Act as a professional market research analyst. I need a comprehensive market research report. Please include an executive summary, competitor analysis, target audience demographics, market size, key trends, marketing strategy and a SWOT analysis. The business idea is: ${userPrompt} if you don't find a business idea in the prompt respond with:
-"DATA NOT FOUND,Data I got: ${userPrompt}" 
+"DATA NOT FOUND" 
 //STRICTLY don't include any other text,code delimiters or anything just the core text in plane paragraph`;
         
         const result = await model.generateContent(prompt)
