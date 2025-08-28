5b8f20d724275d93ca1680bc39cac821b180f78b
GITCDHW <pank36720@gmail.com>
2025-08-28 18:40:34 +0530

    update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
     appId: "1:410394099422:web:4ba469435367567f550c4f",
     measurementId: "G-FQF326L7QB"
   };
-const app = firebase.initializeApp(firebaseConfig);
-const db=firebase.database()
-const rootRef = db.ref("bookList")
-const auth = firebase.auth()
+var app = firebase.initializeApp(firebaseConfig);
+var db=firebase.database()
+var rootRef = db.ref("bookList")
+var auth = firebase.auth()
