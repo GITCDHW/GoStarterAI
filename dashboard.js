9ed34b5620d5348860cdfa7a4e3993156825880e
GITCDHW <pank36720@gmail.com>
2025-08-31 10:09:08 +0530

    update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,9 +1,15 @@
-const urlparams = new URLSearchParams(window.location.search);
-const id = urlparams.get('id');
-const businessRef=db.ref(`users/businesses/${id}`)
-businessRef.once("value").then(snapshot=>{
-  if (snapshot) {
-    const data=snapshot.val()
-    document.getElementById("website-preview-iframe").srcdoc=data.websiteCode
+auth.onAuthStateChanged(user => {
+  if (user) {
+    const urlparams = new URLSearchParams(window.location.search);
+    const id = urlparams.get('id');
+    const businessRef = db.ref(`users/${user.uid}/businesses/${id}`)
+    businessRef.once("value").then(snapshot => {
+      if (snapshot) {
+        const data = snapshot.val()
+        document.getElementById("website-preview-iframe").srcdoc = data.websiteCode
+      }
+    })
+  }else{
+    window.location.href='index.html'
   }
 })
