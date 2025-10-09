b6e12caab3427e36d3b7182d50b48f42501e3d66
GITCDHW <pank36720@gmail.com>
2025-10-09 19:16:03 +0530

    update

--- api/updateFile.js
+++ api/updateFile.js
@@ -2,18 +2,24 @@
 import admin from "firebase-admin";
 import fetch from "node-fetch"; // optional if Node runtime has global fetch
 
-// Build service account from env
 const serviceAccount = {
-  type: "service_account",
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  "type": "service_account",
+  "project_id": "gostarterai",
+  "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  "client_email": "firebase-adminsdk-fbsvc@gostarterai.iam.gserviceaccount.com",
+  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+  "client_id": process.env.FIREBASE_CLIENT_ID,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40gostarterai.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
 };
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.FIREBASE_DATABASE_URL || `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+    databaseURL: "https://gostarterai-default-rtdb.firebaseio.com",
   });
 }
 
